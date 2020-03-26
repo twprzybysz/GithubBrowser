0149341c9723868557019393f437639afc5cb737
Tomasz Przybysz <twprzybysz@gmail.com>
2020-03-26 20:39:51 +0100

    Removed old code

--- GithubBrowser/UI/Views/Login/LoginPresenter.swift
+++ GithubBrowser/UI/Views/Login/LoginPresenter.swift
@@ -37,77 +37,7 @@ final class LoginPresenter {
                 self.view?.presentError(with: "Error", text: error.localizedDescription)
             }
         }
-
-        // SHOW LOADER
-        /*
-        get(with: buildUrl(with: username), decoder: .withIso8601DateDecoder) { [weak self] (reposObj: GithubUser?) in
-            guard let reposObj = reposObj, reposObj.isValid else {
-                self?.view?.presentError(with:  "Error", text: "No user found")
-                return
-            }
-
-            //self?.githubUserResponse = reposObj
-            self?.fetchRepos(with: reposObj.reposURL ?? "") { [weak self] languages in
-                //self?.githubLanguages = languages
-                //UIApplication.shared.isNetworkActivityIndicatorVisible = false
-                //self?.performSegue(withIdentifier: Constants.showResultsSegue, sender: nil)
-            }
-        }
-        */
     }
-
-    /*
-    private func fetchRepos(with reposUrl: String, completion: @escaping (Languages) -> ()) {
-        let languagesGroup = DispatchGroup()
-        let languagesQueue = DispatchQueue(label: "languagesQueue")
-        var languages: Languages = [:]
-
-        languagesGroup.enter()
-        get(with: reposUrl) { [weak self] (reposObj: [GithubUserRepository]?) in
-            reposObj?.forEach { repoObj in
-                languagesGroup.enter()
-
-                self?.get(with: repoObj.languagesUrl) { (languagesObj: Languages?) in
-                    languagesQueue.async(flags: .barrier) {
-                        languages = languagesObj?.reduce(languages, { (result, kv) -> Languages in
-                            var result = result
-                            result[kv.key] = (result[kv.key] ?? 0) + kv.value
-                            return result
-                        }) ?? [:]
-                        languagesGroup.leave()
-                    }
-                }
-            }
-            languagesGroup.leave()
-        }
-
-        languagesGroup.notify(queue: DispatchQueue.main) {
-            completion(languages)
-        }
-    }
-
-    private func get<T: Decodable>(with reposUrl: String, decoder: JSONDecoder = JSONDecoder(), completion: @escaping (T?) -> ()) {
-        guard let url = URL(string: reposUrl) else { return }
-
-        URLSession.shared.dataTask(with: url) { data, _, error in
-             DispatchQueue.main.async {
-                guard let data = data else { return completion(nil) }
-
-                completion(try? decoder.decode(T.self, from: data))
-            }
-        }.resume()
-    }
-
-    private func buildUrl(with username: String) -> String {
-        let parameters = [
-            "client_id": Constants.clientId,
-            "client_secret": Constants.clientSecret
-            ]
-            .map { "\($0.key)=\($0.value)" }
-            .joined(separator: "&")
-        return "\(Constants.baseUrl)\(username)?\(parameters)"
-    }
-    */
 }
 
 extension LoginPresenter: LoginPresenterProtocol {
